b5e6e020de7d10dc3665303e65c1ec8d7d111c23
BlakeJC94 <blakejamescook@gmail.com>
2023-08-19 18:53:37 +1000

    rfc: basic ops tests

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,5 +1,4 @@
 import pytest
-import pandas as pd
 
 import pandas_intervals
 from tests.helpers import (
@@ -14,202 +13,270 @@ from tests.helpers import (
     diff_basic,
 )
 
-cases = [
-    dict(
-        a=[
-            "     (----]    (----]         (--------------]           ",
-        ],
-        b=[
-            " (--]   (---]          (----]    (---]    (------] (---] ",
-        ],
-        a_union_b=[
-            "     (----]    (----]         (--------------]           ",
-            " (--]   (---]          (----]    (---]    (------] (---] ",
-        ],
-        a_intersection_b=[
-            "     (----]                   (--------------]           ",
-            "        (---]                    (---]    (------]       ",
-        ],
-        overlap_a=[
-            "                                                         ",
-        ],
-        overlap_b=[
-            "                                                         ",
-        ],
-        non_overlap_a=[
-            "     (----]    (----]         (--------------]           ",
-        ],
-        non_overlap_b=[
-            " (--]   (---]          (----]    (---]    (------] (---] ",
-        ],
-        combine_a=[
-            "     (----]    (----]         (--------------]           ",
-        ],
-        combine_b=[
-            " (--]   (---]          (----]    (---]    (------] (---] ",
-        ],
-        combine_a_union_b=[
-            " (--](------]  (----]  (----] (------------------] (---] ",
-        ],
-        complement_a=[
-            "          (----]    (---------]                          ",
-        ],
-        complement_b=[
-            "    (---]   (----------]    (----]   (----]      (-]     ",
-        ],
-        a_diff_b=[
-            "     (--]      (----]         (--]   (----]              ",
-        ],
-        b_diff_a=[
-            " (--]     (-]          (----]                (---] (---] ",
-        ],
-    ),
-    dict(
-        a=[
-            "     (----]                                              ",
-            " (--]   (---]          (----]                      (---] ",
-        ],
-        b=[
-            "               (----]         (--------------]           ",
-            "                                 (---]    (------]       ",
-        ],
-        a_union_b=[
-            "     (----]    (----]         (--------------]",
-            " (--]   (---]          (----]    (---]    (------] (---] ",
-        ],
-        a_intersection_b=[
-            "                                                         ",
-        ],
-        overlap_a=[
-            "     (----]                                              ",
-            "        (---]                                            ",
-        ],
-        overlap_b=[
-            "                              (--------------]           ",
-            "                                 (---]    (------]       ",
-        ],
-        non_overlap_a=[
-            " (--]                  (----]                      (---] ",
-        ],
-        non_overlap_b=[
-            "               (----]                                    ",
-        ],
-        combine_a=[
-            " (--](------]          (----]                      (---] ",
-        ],
-        combine_b=[
-            "               (----]         (------------------]       ",
-        ],
-        combine_a_union_b=[
-            " (--](------]  (----]  (----] (------------------] (---] ",
-        ],
-        complement_a=[
-            "    (]      (----------]    (----------------------]     ",
-        ],
-        complement_b=[
-            "                    (---------]                          ",
-        ],
-        a_diff_b=[
-            " (--](------]          (----]                      (---] ",
-        ],
-        b_diff_a=[
-            "               (----]         (------------------]       ",
-        ],
-    ),
-]
+
+@pytest.mark.parametrize(
+    "test_case",
+    [
+        dict(
+            a=[
+                "     (----]    (----]         (--------------]           ",
+            ],
+            b=[
+                " (--]   (---]          (----]    (---]    (------] (---] ",
+            ],
+            a_union_b=[
+                "     (----]    (----]         (--------------]           ",
+                " (--]   (---]          (----]    (---]    (------] (---] ",
+            ],
+        ),
+        dict(
+            a=[
+                "     (----]                                              ",
+                " (--]   (---]          (----]                      (---] ",
+            ],
+            b=[
+                "               (----]         (--------------]           ",
+                "                                 (---]    (------]       ",
+            ],
+            a_union_b=[
+                "     (----]    (----]         (--------------]",
+                " (--]   (---]          (----]    (---]    (------] (---] ",
+            ],
+        ),
+    ],
+)
+def test_union(test_case):
+    df_a = intervals_from_str(test_case["a"])
+    df_b = intervals_from_str(test_case["b"])
+    df_expected = intervals_from_str(test_case["a_union_b"])
+    assert_df_interval_set_equality(df_expected, union_basic(df_a, df_b))
+    assert_df_interval_set_equality(df_expected, union_basic(df_b, df_a))
 
 
-@pytest.mark.parametrize("test_case", cases)
-class TestBasicOps:
-    def test_union(self, test_case):
-        df_a = intervals_from_str(test_case["a"]).ivl()
-        df_b = intervals_from_str(test_case["b"]).ivl()
-        df_expected = intervals_from_str(test_case["a_union_b"]).ivl()
+@pytest.mark.parametrize(
+    "test_case",
+    [
+        dict(
+            a=[
+                "     (----]    (----]         (--------------]           ",
+            ],
+            b=[
+                " (--]   (---]          (----]    (---]    (------] (---] ",
+            ],
+            a_intersection_b=[
+                "     (----]                   (--------------]           ",
+                "        (---]                    (---]    (------]       ",
+            ],
+        ),
+        dict(
+            a=[
+                "     (----]                                              ",
+                " (--]   (---]          (----]                      (---] ",
+            ],
+            b=[
+                "               (----]         (--------------]           ",
+                "                                 (---]    (------]       ",
+            ],
+            a_intersection_b=[
+                "                                                         ",
+            ],
+        ),
+    ],
+)
+def test_intersection(test_case):
+    df_a = intervals_from_str(test_case["a"])
+    df_b = intervals_from_str(test_case["b"])
+    df_expected = intervals_from_str(test_case["a_intersection_b"])
+    assert_df_interval_set_equality(df_expected, intersection_basic(df_a, df_b))
+    assert_df_interval_set_equality(df_expected, intersection_basic(df_b, df_a))
 
-        assert_df_interval_set_equality(df_expected, union_basic(df_a, df_b))
-        assert_df_interval_set_equality(df_expected, union_basic(df_b, df_a))
 
-    def test_intersection(self, test_case):
-        df_a = intervals_from_str(test_case["a"]).ivl()
-        df_b = intervals_from_str(test_case["b"]).ivl()
-        df_expected = (
-            intervals_from_str(test_case["a_intersection_b"]).ivl()
-        )
+@pytest.mark.parametrize(
+    "test_case",
+    [
+        dict(
+            a=[
+                "     (----]    (----]         (--------------]           ",
+            ],
+            overlap_a=[
+                "                                                         ",
+            ],
+            non_overlap_a=[
+                "     (----]    (----]         (--------------]           ",
+            ],
+        ),
+        dict(
+            a=[
+                "               (----]         (--------------]           ",
+                "                                 (---]    (------]       ",
+            ],
+            overlap_a=[
+                "                              (--------------]           ",
+                "                                 (---]    (------]       ",
+            ],
+            non_overlap_a=[
+                "               (----]                                    ",
+            ],
+        ),
+        dict(
+            a=[
+                "     (----]                                              ",
+                " (--]   (---]          (----]                      (---] ",
+            ],
+            overlap_a=[
+                "     (----]                                              ",
+                "        (---]                                            ",
+            ],
+            non_overlap_a=[
+                " (--]                  (----]                      (---] ",
+            ],
+        ),
+    ],
+)
+def test_overlap(test_case):
+    df_a = intervals_from_str(test_case["a"])
+    df_expected_overlap_a = intervals_from_str(test_case["overlap_a"])
+    df_expected_non_overlap_a = intervals_from_str(test_case["non_overlap_a"])
+    assert_df_interval_set_equality(df_expected_overlap_a, overlap_basic(df_a))
+    assert_df_interval_set_equality(df_expected_non_overlap_a, non_overlap_basic(df_a))
 
-        assert_df_interval_set_equality(df_expected, intersection_basic(df_a, df_b))
-        assert_df_interval_set_equality(df_expected, intersection_basic(df_b, df_a))
 
-    def test_overlap(self, test_case):
-        df_a = intervals_from_str(test_case["a"]).ivl()
-        df_b = intervals_from_str(test_case["b"]).ivl()
-        df_expected_a = (
-            intervals_from_str(test_case["overlap_a"]).ivl()
-        )
-        df_expected_b = (
-            intervals_from_str(test_case["overlap_b"]).ivl()
-        )
+@pytest.mark.parametrize(
+    "test_case",
+    [
+        dict(
+            a=[
+                "     (----]    (----]         (--------------]           ",
+            ],
+            combine_a=[
+                "     (----]    (----]         (--------------]           ",
+            ],
+        ),
+        dict(
+            a=[
+                "               (----]         (--------------]           ",
+                "                                 (---]    (------]       ",
+            ],
+            combine_a=[
+                "               (----]         (------------------]       ",
+            ],
+        ),
+        dict(
+            a=[
+                "     (----]    (----]         (--------------]           ",
+                " (--]   (---]          (----]    (---]    (------] (---] ",
+            ],
+            combine_a=[
+                " (--](------]  (----]  (----] (------------------] (---] ",
+            ],
+        ),
+        dict(
+            a=[
+                "     (----]                                              ",
+                " (--]   (---]          (----]                      (---] ",
+            ],
+            combine_a=[
+                " (--](------]          (----]                      (---] ",
+            ],
+        ),
+    ],
+)
+def test_combine(test_case):
+    df_a = intervals_from_str(test_case["a"])
+    df_expected_combine_a = intervals_from_str(test_case["combine_a"])
+    assert_df_interval_set_equality(df_expected_combine_a, combine_basic(df_a))
 
-        assert_df_interval_set_equality(df_expected_a, overlap_basic(df_a))
-        assert_df_interval_set_equality(df_expected_b, overlap_basic(df_b))
 
-    def test_non_overlap(self, test_case):
-        df_a = intervals_from_str(test_case["a"]).ivl()
-        df_b = intervals_from_str(test_case["b"]).ivl()
-        df_expected_a = (
-            intervals_from_str(test_case["non_overlap_a"]).ivl()
-        )
-        df_expected_b = (
-            intervals_from_str(test_case["non_overlap_b"]).ivl()
-        )
+@pytest.mark.parametrize(
+    "test_case",
+    [
+        dict(
+            a=[
+                "     (----]    (----]         (--------------]           ",
+            ],
+            complement_a=[
+                "          (----]    (---------]                          ",
+            ],
+        ),
+        dict(
+            a=[
+                " (--]   (---]          (----]    (---]    (------] (---] ",
+            ],
+            complement_a=[
+                "    (---]   (----------]    (----]   (----]      (-]     ",
+            ],
+        ),
+        dict(
+            a=[
+                "               (----]         (--------------]           ",
+                "                                 (---]    (------]       ",
+            ],
+            complement_a=[
+                "                    (---------]                          ",
+            ],
+        ),
+        dict(
+            a=[
+                "     (----]    (----]         (--------------]           ",
+                " (--]   (---]          (----]    (---]    (------] (---] ",
+            ],
+            complement_a=[
+                "    (]      (--]    (--]    (-]                  (-]     ",
+            ],
+        ),
+    ],
+)
+def test_complement(test_case):
+    df_a = intervals_from_str(test_case["a"])
+    df_expected_a = intervals_from_str(test_case["complement_a"])
+    assert_df_interval_set_equality(df_expected_a, complement_basic(df_a))
 
-        assert_df_interval_set_equality(df_expected_a, non_overlap_basic(df_a))
-        assert_df_interval_set_equality(df_expected_b, non_overlap_basic(df_b))
 
-    def test_combine(self, test_case):
-        df_a = intervals_from_str(test_case["a"]).ivl()
-        df_b = intervals_from_str(test_case["b"]).ivl()
-        df_expected_combine_a = (
-            intervals_from_str(test_case["combine_a"]).ivl()
-        )
-        df_expected_combine_b = (
-            intervals_from_str(test_case["combine_b"]).ivl()
-        )
-        df_expected_combine_a_union_b = (
-            intervals_from_str(test_case["combine_a_union_b"]).ivl()
-        )
-
-        assert_df_interval_set_equality(
-            df_expected_combine_a, combine_basic(df_a)
-        )
-        assert_df_interval_set_equality(
-            df_expected_combine_b, combine_basic(df_b)
-        )
-        assert_df_interval_set_equality(
-            df_expected_combine_a_union_b, combine_basic(union_basic(df_a, df_b))
-        )
-
-    def test_complement(self, test_case):
-        df_a = intervals_from_str(test_case["a"]).ivl()
-        df_b = intervals_from_str(test_case["b"]).ivl()
-        df_expected_a = (
-            intervals_from_str(test_case["complement_a"]).ivl()
-        )
-        df_expected_b = (
-            intervals_from_str(test_case["complement_b"]).ivl()
-        )
-
-        assert_df_interval_set_equality(df_expected_a, complement_basic(df_a))
-        assert_df_interval_set_equality(df_expected_b, complement_basic(df_b))
-
-    def test_diff(self, test_case):
-        df_a = intervals_from_str(test_case["a"]).ivl()
-        df_b = intervals_from_str(test_case["b"]).ivl()
-        df_expected_a_diff_b = (
-            intervals_from_str(test_case["a_diff_b"]).ivl()
-        )
-        df_expected_b_diff_a = (
-            intervals_from_str(test_case["b_diff_a"]).ivl()
-        )
-
-        assert_df_interval_set_equality(df_expected_a_diff_b, diff_basic(df_a, df_b))
-        assert_df_interval_set_equality(df_expected_b_diff_a, diff_basic(df_b, df_a))
+@pytest.mark.parametrize(
+    "test_case",
+    [
+        dict(
+            a=[
+                "     (----]    (----]         (--------------]           ",
+            ],
+            b=[
+                " (--]   (---]          (----]    (---]    (------] (---] ",
+            ],
+            a_diff_b=[
+                "     (--]      (----]         (--]   (----]              ",
+            ],
+            b_diff_a=[
+                " (--]     (-]          (----]                (---] (---] ",
+            ],
+        ),
+        dict(
+            a=[
+                "     (----]                                              ",
+                " (--]   (---]          (----]                      (---] ",
+            ],
+            b=[
+                "               (----]         (--------------]           ",
+                "                                 (---]    (------]       ",
+            ],
+            complement_a=[
+                "    (]      (----------]    (----------------------]     ",
+            ],
+            complement_b=[
+                "                    (---------]                          ",
+            ],
+            a_diff_b=[
+                " (--](------]          (----]                      (---] ",
+            ],
+            b_diff_a=[
+                "               (----]         (------------------]       ",
+            ],
+        ),
+    ],
+)
+def test_diff(test_case):
+    df_a = intervals_from_str(test_case["a"])
+    df_b = intervals_from_str(test_case["b"])
+    df_expected_a_diff_b = intervals_from_str(test_case["a_diff_b"])
+    df_expected_b_diff_a = intervals_from_str(test_case["b_diff_a"])
+    assert_df_interval_set_equality(df_expected_a_diff_b, diff_basic(df_a, df_b))
+    assert_df_interval_set_equality(df_expected_b_diff_a, diff_basic(df_b, df_a))
