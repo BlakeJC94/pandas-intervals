a75b8be10f1ede9e67371967f4b5b70244a1812c
BlakeJC94 <blakejamescook@gmail.com>
2023-08-24 07:32:07 +1000

    rfc: simplify nearest_basic

--- tests/helpers.py
+++ tests/helpers.py
@@ -347,16 +347,24 @@ def nearest_basic(
     df_b: pd.DataFrame,
 ) -> pd.DataFrame:
     intervals_a = df_to_list(df_a)
-    starts_b, ends_b = df_b[["start", "end"]].to_numpy().T
+    intervals_b = df_to_list(df_b)
 
     results = []
     for start_a, end_a, *_ in intervals_a:
-        result = np.concatenate(
-            [
-                start_a - ends_b[ends_b <= start_a],
-                starts_b[end_a <= starts_b] - end_a,
-            ]
-        )
-        results.append(result.min())
+        min_dist = float("inf")
+        for start_b, end_b, *_ in intervals_b:
+            if end_b <= start_a:
+                min_dist = min(min_dist, start_a - end_b)
+            if end_a <= start_b:
+                min_dist = min(min_dist, start_b - end_a)
+            # Intersections should result in zero distances
+            # if (
+            #     (start_a < start_b < end_a)
+            #     or (start_a < end_b < end_a)
+            #     or (start_b < start_a < end_b)
+            #     or (start_b < end_a < end_b)
+            # ):
+            #     min_dist = 0
+        results.append(min_dist)
 
     return df_a.assign(min_dist=results)
