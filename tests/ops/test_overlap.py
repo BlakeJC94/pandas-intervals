f3c4f5615d1d48c36813cea8a8c5f6193c21727b
BlakeJC94 <blakejamescook@gmail.com>
2023-09-02 16:49:18 +1000

    test: overlap edge cases

--- tests/ops/test_overlap.py
+++ tests/ops/test_overlap.py
@@ -8,9 +8,13 @@ from tests.helpers import (
     intervals_from_str,
 )
 
+
 @pytest.mark.parametrize(
     "operation, operation_non",
-    [(overlap_basic, non_overlap_basic), (intervals_overlap, intervals_non_overlap)],
+    [
+        (overlap_basic, non_overlap_basic),
+        (intervals_overlap, intervals_non_overlap),
+    ],
 )
 class TestIntervalsOverlap:
     @staticmethod
@@ -19,22 +23,20 @@ class TestIntervalsOverlap:
         df_expected_overlap_a = intervals_from_str(test_case["overlap_a"])
         df_expected_non_overlap_a = intervals_from_str(test_case["non_overlap_a"])
         assert_df_interval_set_equality(df_expected_overlap_a, operations[0](df_a))
-        assert_df_interval_set_equality(
-            df_expected_non_overlap_a, operations[1](df_a)
-        )
+        assert_df_interval_set_equality(df_expected_non_overlap_a, operations[1](df_a))
 
     @pytest.mark.parametrize(
         "test_case",
         [
             dict(
                 a=[
-                    "     (----]    (----]         (--------------]           ",
+                    "     (----]    (----]   |     (--------------]           ",
                 ],
                 overlap_a=[
                     "                                                         ",
                 ],
                 non_overlap_a=[
-                    "     (----]    (----]         (--------------]           ",
+                    "     (----]    (----]   |     (--------------]           ",
                 ],
             ),
         ],
@@ -78,4 +80,38 @@ class TestIntervalsOverlap:
     def test_it_returns_overlaps_when_arg_has_overlaps(
         self, test_case, operation, operation_non
     ):
+        self.check_operations((operation, operation_non), test_case)
+
+    @pytest.mark.parametrize(
+        "test_case",
+        [
+            dict(
+                a=[
+                    "       (---]   (----]                                    ",
+                    "  (----]            (---]                                ",
+                ],
+                overlap_a=[
+                    "                                                         ",
+                ],
+                non_overlap_a=[
+                    "       (---]   (----]                                    ",
+                    "  (----]            (---]                                ",
+                ],
+            ),
+            dict(  # TODO Revisit right point overlap logic
+                a=[
+                    "       (---]   (----]      |                             ",
+                    "       |            |      |                             ",
+                ],
+                overlap_a=[
+                    "                                                         ",
+                ],
+                non_overlap_a=[
+                    "       (---]   (----]      |                             ",
+                    "       |            |      |                             ",
+                ],
+            ),
+        ],
+    )
+    def test_it_handles_edges(self, test_case, operation, operation_non):
         self.check_operations((operation, operation_non), test_case)
