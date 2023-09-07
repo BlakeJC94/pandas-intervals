0e8e64dc78c41f13b17621d0787859ebf3abc107
BlakeJC94 <blakejamescook@gmail.com>
2023-09-07 16:40:43 +1000

    rfc: basic intervals intersect

--- pandas_intervals/ops/basic/intersection.py
+++ pandas_intervals/ops/basic/intersection.py
@@ -1,9 +1,19 @@
 from itertools import product
+from typing import Tuple
 
 import pandas as pd
 
 from pandas_intervals.utils import df_to_list
 
+def _intervals_intersect(ivl_a: Tuple[float, float], ivl_b: Tuple[float, float]) -> bool:
+    start_a, end_a, *_ = ivl_a
+    start_b, end_b, *_ = ivl_b
+    return (
+        (start_a < start_b < end_a)
+        or (start_a < end_b < end_a)
+        or (start_b < start_a < end_b)
+        or (start_b < end_a < end_b)
+    )
 
 def intersection(df_a: pd.DataFrame, df_b: pd.DataFrame) -> pd.DataFrame:
     cols = df_a.columns
@@ -12,14 +22,11 @@ def intersection(df_a: pd.DataFrame, df_b: pd.DataFrame) -> pd.DataFrame:
 
     result = set()
     for ivl_a, ivl_b in product(intervals_a, intervals_b):
-        start_a, end_a, *_ = ivl_a
-        start_b, end_b, *_ = ivl_b
-        if (
-            (start_a < start_b < end_a)
-            or (start_a < end_b < end_a)
-            or (start_b < start_a < end_b)
-            or (start_b < end_a < end_b)
-        ):
+        if _intervals_intersect(ivl_a, ivl_b):
+            result.add(ivl_a)
+            result.add(ivl_b)
+
+    return pd.DataFrame(result, columns=cols).sort_values(["start", "end"])
             result.add(ivl_a)
             result.add(ivl_b)
 
